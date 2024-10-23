32f066ed43d6ed7277b12a43baabec678dc6f8a7
Muzaffar-tech <muzaffaromonov10@gmail.com>
2024-10-23 05:22:55 +0500

    initial commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import GenericAPIView
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status, mixins
 from .models import Food, FoodType, Comment
