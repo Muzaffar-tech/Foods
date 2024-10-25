0bb3048922dda3a6349959b3f76f2ac262631a60
Muzaffar-tech <muzaffaromonov10@gmail.com>
2024-10-25 06:54:29 +0500

    initial commit

--- app/urls.py
+++ app/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path('foods/', FoodListApiView.as_view(), name='food-list'),
     path('foods/<int:pk>/', FoodDetailApiView.as_view(), name='food-detail'),
 
+    path('type/<int:color_id>/food/', FoodListApiView.as_view()),
+
     path('comments/', CommentListApiView.as_view(), name='comment-list'),
     path('comments/<int:pk>', CommentDetailApiView.as_view(), name='comment-detail')
 
--- app/views.py
+++ app/views.py
@@ -1,102 +1,58 @@
-from rest_framework.generics import GenericAPIView
+from rest_framework import generics, filters
 from rest_framework.response import Response
 from rest_framework import status, mixins
 from .models import Food, FoodType, Comment
 from .serializers import FoodSerializer, FoodTypeSerializer, CommentSerializer
 from rest_framework.permissions import IsAuthenticated
 
-class FoodTypeListApiView(GenericAPIView):
+class FoodTypeListApiView(generics.ListCreateAPIView):
     serializer_class = FoodTypeSerializer
     queryset = FoodType.objects.all()
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    ordering_fields = ['pk', 'name']
+    search_fields = ['name']
 
-    def get(self, request):
-        food_types = self.get_queryset()
-        serializer = self.get_serializer(food_types, many=True)
-        return Response(serializer.data)
 
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class FoodListApiView(GenericAPIView):
+class FoodListApiView(generics.ListCreateAPIView):
     serializer_class = FoodSerializer
     queryset = Food.objects.all()
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    ordering_fields = ['pk', 'name']
+    search_fields = ['name', 'composition']
 
-    def get(self, request):
-        foods = self.get_queryset()
-        serializer = self.get_serializer(foods, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        if self.kwargs.get("food_type_id", False):
+            foods = Food.objects.filter(food_type_id=self.kwargs.get("color_id"))
+        else:
+            foods = Food.objects.all()
+        return foods
 
-    def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class CommentListApiView(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
+
+
+
+class CommentListApiView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    ordering_fields = ['pk', 'name']
+    search_fields = ['text', 'food', 'author']
+
+
+class CommentDetailApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class CommentDetailApiView(mixins.RetrieveModelMixin,
-                        mixins.UpdateModelMixin,
-                        mixins.DestroyModelMixin,
-                        GenericAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-class FoodTypeDetailApiView(mixins.RetrieveModelMixin,
-                         mixins.UpdateModelMixin,
-                         mixins.DestroyModelMixin,
-                         GenericAPIView):
+class FoodTypeDetailApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = FoodType.objects.all()
     serializer_class = FoodTypeSerializer
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-class FoodDetailApiView(mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.DestroyModelMixin,
-                     GenericAPIView):
+class FoodDetailApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     permission_classes = [IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
